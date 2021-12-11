3a1f938d43aa9f3da90b41c92b4cb1b0850ba720
ts-bill <tanawit_7@hotmail.com>
2021-12-12 00:07:52 +0700

    edit label gen

--- createYolo5data.py
+++ createYolo5data.py
@@ -38,9 +38,9 @@ def write_textfile():
         
         #open('labels/%s.txt' % i['image_id'], 'w').write(str(i['category_id']-1)+" "+str(i['bbox'][0])+" "+str(i['bbox'][1])+" "+str(i['bbox'][2])+" "+str(i['bbox'][3]))
         if i['image_id'] in dict_nine:
-            dict_nine[i['image_id']].append(str(i['category_id']-1)+" "+str(i['bbox'][0]*0.59259259259259)+" "+str(i['bbox'][1]*0.33333333)+" "+str(i['bbox'][2]*0.59259259259259)+" "+str(i['bbox'][3]*0.33333333))
+            dict_nine[i['image_id']].append(str(i['category_id']-1)+" "+str((i['bbox'][0]*0.59259259259259)/640)+" "+str((i['bbox'][1]*0.33333333)/640)+" "+str((i['bbox'][2]*0.59259259259259)/640)+" "+str((i['bbox'][3]*0.33333333)/640))
         else:    
-            dict_nine[i['image_id']] = [str(i['category_id']-1)+" "+str(i['bbox'][0]*0.59259259259259)+" "+str(i['bbox'][1]*0.33333333)+" "+str(i['bbox'][2]*0.59259259259259)+" "+str(i['bbox'][3]*0.33333333)]
+            dict_nine[i['image_id']] = [str(i['category_id']-1)+" "+str((i['bbox'][0]*0.59259259259259)/640)+" "+str((i['bbox'][1]*0.33333333)/640)+" "+str((i['bbox'][2]*0.59259259259259)/640)+" "+str((i['bbox'][3]*0.33333333)/640)]
     
     for key, val in dict_nine.items():
         open('labels/%s.txt' % key, 'w').write("\n".join(str(it) for it in val))
